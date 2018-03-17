7c5b1b1987102d6e6aa52e72eebad2ae3f779e77
ishi-bhatnagar <35772166+ishi-bhatnagar@users.noreply.github.com>
2018-03-17 19:01:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML='New';
+
+var img=document.getElementById('image');
+img.onclick=function(){
+    img.style.marginLeft ='100px';
+}
