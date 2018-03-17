05429439ba6d9f0c1ad009f31a107723c662eecc
ishi-bhatnagar <35772166+ishi-bhatnagar@users.noreply.github.com>
2018-03-17 19:07:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,14 @@ var element = document.getElementById('main-text');
 element.innerHTML='New';
 
 var img=document.getElementById('image');
+var marginLeft=0;
+
+function moveRight()
+{
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
 img.onclick=function(){
-    img.style.marginLeft ='100px';
+    var interval = setInterval(moveRight,100);
 }
