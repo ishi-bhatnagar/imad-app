770e4194f40679a952f5f1d13bc2565740ba02d8
ishi-bhatnagar <35772166+ishi-bhatnagar@users.noreply.github.com>
2018-03-19 19:36:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,9 +18,9 @@ img.onclick=function(){
 
 var button = document.getElementById('counter');
 var counter = 0;
-button.onClick = function(){
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML= counter.toString();
+button.onclick = function (){
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
     
 };
